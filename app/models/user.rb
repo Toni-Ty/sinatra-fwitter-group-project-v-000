177bc41f553ca081bf953961481704c3d066d4ab
Toni-Ty <antonique2479@yahoo.com>
2019-12-07 04:26:43 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ActiveRecord::Base
 end
 
 
-  def slug
-    username.downcase.gsub(" ","-"))
-  end
+has_secure_password
 
-  def self.find_by_slug(slug)
-    User.all.find { |user| user.slug == slug}
-  end
+def slug
+  username.downcase.gsub(" ","-")
+end
+
+def self.find_by_slug(slug)
+  User.all.find{|user| user.slug == slug}
+end
+
 end
