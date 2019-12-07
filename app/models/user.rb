f951c6d26df3c7af84dfb69e6171fa64b5e76184
Toni-Ty <antonique2479@yahoo.com>
2019-12-07 04:40:52 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :tweets
-
-
   has_secure_password
+  has_many :tweets
+end
+
 
 def slug
   username.downcase.gsub(" ","-")
@@ -10,5 +10,4 @@ end
 
 def self.find_by_slug(slug)
   User.all.find{|user| user.slug == slug}
-end
 end
