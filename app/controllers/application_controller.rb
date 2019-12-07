35f12b43f4f063b732d8d5127b3103d5691acd97
Toni-Ty <antonique2479@yahoo.com>
2019-12-07 04:36:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,30 @@
 require './config/environment'
 #The ( :: ) symbol below is a unary operator that allows: constants, instance methods and class methods defined within a class or module, to be accessed from anywhere outside the class or module.
 # Base class inside the ActiveRecord.Module
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
-  enable :sessions
 
   configure do
-    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
-get '/' do
-   erb :index
- end
+  get '/' do
+    erb :index
+  end
 
+  helpers do
 
- helpers do
+    def logged_in?
+      !!current_user
+    end
 
-   def logged_in?
-     !!current_user
-   end
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 
- def current_user
-   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-   end
- end
-
-
-
+  end
 end
