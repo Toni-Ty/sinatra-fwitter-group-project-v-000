d862da820293647ec4bf1a868b7463ea09065b94
Toni-Ty <antonique2479@yahoo.com>
2019-12-07 04:40:33 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tweets
-end
+
 
   has_secure_password
 
@@ -10,4 +10,5 @@ end
 
 def self.find_by_slug(slug)
   User.all.find{|user| user.slug == slug}
+end
 end
